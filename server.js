7c1b6f18bfeae9a437eaa740c807fd9658d62953
younisahmad <30660582+younisahmad@users.noreply.github.com>
2017-08-09 23:46:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
-app.get('/ist', function(req,res){
+app.get('/', function(req,res){
    res.send("first article"); 
 });
 app.get('/2nd', function(req,res){
